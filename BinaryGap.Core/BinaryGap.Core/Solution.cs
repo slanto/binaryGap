cb93da8cc6a1580ccb2a5af63342c74d7aed184b
slanto <slawek.antos@outlook.com>
2017-01-03 16:00:41 +0100

    BinaryGap implementation with unit tests

---
+++ BinaryGap.Core/BinaryGap.Core/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace BinaryGap.Core
+{
+    using System;
+
+    public class Solution
+    {
+        public int BinaryGap(int n)
+        {
+            var binary = Convert.ToString(n, 2).Trim('0');
+            var gaps = binary.Split('1');
+            Array.Sort(gaps);
+            return gaps[gaps.Length-1].Length;
+        }
+    }
+}
---
+++ BinaryGap.Core/BinaryGap.Test/SolutionTests.cs
@@ -0,0 +1,33 @@
+﻿namespace BinaryGap.Test
+{
+    using BinaryGap.Core;
+
+    using Xunit;
+        
+    public class SolutionTests
+    {
+        [Theory()]
+        [InlineData(1041, 5)]
+        [InlineData(9, 2)]
+        [InlineData(15, 0)]
+        [InlineData(2147483647, 0)]
+        [InlineData(6, 0)]
+        [InlineData(5, 1)]
+        [InlineData(16, 0)]
+        [InlineData(1024, 0)]
+        [InlineData(328, 2)]
+        [InlineData(51712, 2)]
+        [InlineData(20, 1)]
+        public void BinaryGap_ShouldReturnCorrectValueForGivenInput(int value, int result)
+        {
+            //Arange
+            var sut = new Solution();
+
+            //Act
+            var actual = sut.BinaryGap(value);
+
+            //Assert
+            Assert.Equal(result, actual);
+        }
+    }
+}
